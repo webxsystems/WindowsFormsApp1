d3c33032abfa40bdd6664515925d14512b69f2d3
David Burney <david@acromove.net>
2020-09-22 15:28:38 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.displayText = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(398, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Sheet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(531, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // displayText
+            // 
+            this.displayText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.displayText.Font = new System.Drawing.Font("Agency FB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.displayText.ForeColor = System.Drawing.Color.Lime;
+            this.displayText.Location = new System.Drawing.Point(272, 138);
+            this.displayText.Multiline = true;
+            this.displayText.Name = "displayText";
+            this.displayText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.displayText.Size = new System.Drawing.Size(386, 197);
+            this.displayText.TabIndex = 2;
+            this.displayText.TextChanged += new System.EventHandler(this.displayText_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(517, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 30);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusBox
+            // 
+            this.statusBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.statusBox.Font = new System.Drawing.Font("Agency FB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.statusBox.ForeColor = System.Drawing.Color.Aqua;
+            this.statusBox.Location = new System.Drawing.Point(17, 138);
+            this.statusBox.Multiline = true;
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(238, 197);
+            this.statusBox.TabIndex = 4;
+            this.statusBox.Text = "Initializing...";
+            this.statusBox.TextChanged += new System.EventHandler(this.statusBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(20, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Status";
+            // 
+            // fileName
+            // 
+            this.fileName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.fileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fileName.ForeColor = System.Drawing.Color.White;
+            this.fileName.Location = new System.Drawing.Point(271, 74);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(388, 23);
+            this.fileName.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(272, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Filename";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(277, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Output";
+            // 
+            // messageBox
+            // 
+            this.messageBox.BackColor = System.Drawing.Color.Black;
+            this.messageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.messageBox.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.messageBox.ForeColor = System.Drawing.Color.White;
+            this.messageBox.Location = new System.Drawing.Point(20, 12);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.messageBox.Size = new System.Drawing.Size(418, 23);
+            this.messageBox.TabIndex = 9;
+            this.messageBox.Text = "Click the \'Open Sheet\' button and select file to process.";
+            this.messageBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ebrima", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Silver;
+            this.label4.Location = new System.Drawing.Point(607, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 11);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "POWERED BY";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(679, 389);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.displayText);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Upstate Pricing Update";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox displayText;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox statusBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.Exporting.Text;
+using Spire.Pdf.General.Find;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                this.statusBox.Text += Environment.NewLine + "Selecting file..." + Environment.NewLine;
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "pdf files (*.pdf)|*.pdf|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.messageBox.Text = " ";
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            this.statusBox.Text += "Processing PDF file..." + Environment.NewLine;
+            this.fileName.Text = filePath;
+
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(filePath);
+            PdfPageBase page = doc.Pages[0];
+            SimpleTextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+            string text = page.ExtractText(strategy);
+
+            //PdfTextFind[] results = null;
+
+            //results = page.FindText("MS61").Finds;
+
+            //foreach (PdfTextFind texts in results)
+            //{
+            //    PointF p = texts.Position;
+            //    float x = texts.Position.X;
+            //    float y = texts.Position.Y;
+
+            //   // this.displayText.Text += p + Environment.NewLine;
+            //    string textArea = page.ExtractText(new RectangleF(x-30, y, 80, 180));
+            //  //  this.displayText.Text += Environment.NewLine + textArea + Environment.NewLine;
+            //}
+
+
+
+            //MessageBox.Show(text, " ", MessageBoxButtons.OK);
+
+            //Document pdfDocument = new Document(filePath);
+
+            //TextAbsorber textAbsorber = new TextAbsorber();
+
+            //pdfDocument.Pages.Accept(textAbsorber);
+
+            //string extractedText = textAbsorber.Text;
+
+            //Console.WriteLine(extractedText);
+
+            //MessageBox.Show(extractedText, "Upstate Gold Sheet", MessageBoxButtons.OK);
+
+            //PdfDocument PDF = PdfDocument.FromFile(filePath);
+            //string AllText = PDF.ExtractAllText();
+
+
+            string[] stringArray = text.Split(Environment.NewLine);
+
+            //string[] stringArray = text.Split("$");
+            this.statusBox.Text += "API Request/Response..." + Environment.NewLine;
+
+            foreach (string line in stringArray)
+            {
+                 bool stringExists = line.Contains("$");
+                if (stringExists)
+                {
+                    string[] lineSplit = line.Split(' ');
+                //  foreach ( string word in lineSplit)
+                //                    {
+                //   this.displayText.Text += word + Environment.NewLine;
+                //                    }
+                //                   this.displayText.Text += line + Environment.NewLine;
+                //  this.displayText.Text += " = " + lineSplit[3] + Environment.NewLine;
+ 
+                    string requestString = "method=test&requestString=" + line;
+                    this.displayText.Text += "--> " + lineSplit[0] + " " + lineSplit[1];
+                    string responseString = APIRequest(requestString);
+                    string[] responseArray = responseString.Split(':');
+                    responseArray[1] = responseArray[1].Replace("}", string.Empty);
+                   // MessageBox.Show(responseArray[1], "api response");
+                    if(responseArray[1] == "0") {
+                        this.displayText.Text += ": Success" + Environment.NewLine;
+                    }
+                    else
+                    {
+                     //   this.displayText.ForeColor
+                        this.displayText.Text += ": Failure" + Environment.NewLine;
+                    }
+//                    this.displayText.Text += responseString + Environment.NewLine;
+                }
+            }
+
+
+            this.messageBox.Text = "Click the 'Exit' button to end the application.";
+            this.statusBox.Text += "Job End...";
+           // MessageBox.Show(fileContent, "Unconverted Content " + filePath, MessageBoxButtons.OK);
+
+          // MessageBox.Show(AllText, "Upstate Gold Sheet", MessageBoxButtons.OK);
+           // MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+
+            // Show the dialog and get result.
+            //DialogResult result = openFileDialog1.ShowDialog();
+            //if (result == DialogResult.OK) // Test result.
+            //{
+            //}
+            // Console.WriteLine(result); // <-- For debugging use.
+        }
+
+        public string APIRequest(string requestString)
+        {
+            //
+            // format rest_server/REST/price.class.php?method=updatePrice&product=10Liberty&price=1874
+            //
+            string requestUrl = "http://localhost/rest_server/REST/price.class.php";
+            HttpWebRequest request = HttpWebRequest.CreateHttp(requestUrl);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            using ( var writer = new StreamWriter(request.GetRequestStream()))
+            {
+                writer.Write(requestString);
+            }
+
+            string responseFromRemoteServer;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using( StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    responseFromRemoteServer = reader.ReadToEnd();
+                }
+            }
+
+            return responseFromRemoteServer;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        public class ExtractTextAll
+        {
+            public static void Run()
+            {
+
+            }
+        }
+
+        private void displayText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
